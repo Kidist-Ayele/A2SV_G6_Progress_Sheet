48af525f3f716d2c6df59d2eff1ef4f4b153e235
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2025-05-02 15:33:00 +0300

    Time: 1 ms (68.27%), Space: 18.5 MB (25.56%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,29 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        parent_x = self.find(x)
+        parent_y = self.find(y)
+
+        if parent_x == parent_y:
+            return False
+
+        self.parent[parent_y] = parent_x
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        uf = UnionFind(n)
+
+        for u, v in edges:
+            if not uf.union(u, v):
+                return [u, v]
+
+        
