68eeb63194d9891da3fb13dd0366a6c920b87636
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2025-08-18 14:19:12 +0300

    Time: 1 ms (47.86%), Space: 18 MB (23.85%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,14 +1,16 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         count = Counter(arr1)
-        arr1.sort()
-        arr2_set = set(arr2)
-        res = []
+        num_set = set(arr2)
+        result = []
         for num in arr2:
-            res.extend([num]*count[num])
-        for x in arr1:
-            if x not in arr2_set:
-                res.append(x)
-        return res
-
-        
+            temp = [num] * count[num]
+            result.extend(temp)
+        remain = []
+        for num in arr1:
+            if num not in num_set:
+                remain.append(num)
+                
+        remain.sort()
+        result.extend(remain)
+        return result
