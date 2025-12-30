e21319ed2457fbd535b8a0227408915f35161945
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2025-12-30 19:18:24 +0300

    Time: 0 ms (100%), Space: 18.4 MB (94.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,37 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def lower_bound(nums):
-            left, right = 0, len(nums)
-
-            while left < right:
-                mid = left + (right - left) // 2
-
+        result = [-1, -1]
+        def first(target):
+            left, right = 0, len(nums) - 1
+            
+            while left <= right:
+                mid = (right + left) // 2
                 if nums[mid] < target:
                     left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    right = mid
-            return left
+                    result[0] = mid
+                    right = mid - 1
 
-        def upper_bound(nums):
-            left, right = 0, len(nums)
-
-            while left < right:
-                mid = left + (right - left) // 2
-
-                if nums[mid] <= target:
+        def last(target):
+            left, right = 0, len(nums) - 1
+            
+            while left <= right:
+                mid = (right + left) // 2
+                if nums[mid] < target:
                     left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    right = mid
-            return left
-
-        ans = [-1, -1]
-        left_idx = lower_bound(nums)
-        right_idx = upper_bound(nums) - 1
-
-        if left_idx < len(nums) and nums[left_idx] == target:
-            ans[0] = left_idx 
-        if 0 <= right_idx and nums[right_idx] == target:
-            ans[1] = right_idx 
-        return ans
-        
-        
+                    result[1] = mid
+                    left = mid + 1
+        first(target)
+        last(target)
+        return result
