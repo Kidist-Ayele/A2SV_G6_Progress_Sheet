44c2aac47b1520284c7aa876d7900ea557777ee3
Kidist Ayele <138240613+Kidist-Ayele@users.noreply.github.com>
2026-02-23 10:11:01 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,33 +1,15 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
         nums.sort()
-        mod = 10 ** 9 + 7
-        ans = 0
+        res = 0
+        mod = (10**9 + 7)
+        r = len(nums) - 1
 
-        # Precompute power array to get the number of subsquence in O(1) time
-        power = [1] * len(nums)
-
-        for i in range(1,len(nums)):
-            power[i] = power[i - 1] * 2
-
-        for left in range(len(nums)):
-            right = self.upper_bound(nums, left, target - nums[left])
-            if right >= left:
-                ans = (ans + power[right - left]) % mod
-        return ans
-
-    def upper_bound(self, nums, left, target):
-        left, right = left, len(nums) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-
-            if nums[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return right
+        for i, left in enumerate(nums):
+            while (left + nums[r]) > target and i <= r:
+                r -= 1
+            if i <= r:
+                res += (2**(r - i)) % mod
+        return res
 
 
-
-        
